9f9b754ef1e60755597f57474a1558dde49a08cc
ldj <1093987950@qq.com>
2021-12-23 16:36:30 +0800

    ldj

--- src/main/java/com/TonySun/demo/controller/HelloWorld.java
+++ src/main/java/com/TonySun/demo/controller/HelloWorld.java
@@ -2,6 +2,6 @@ package com.TonySun.demo.controller;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld1");
     }
 }
